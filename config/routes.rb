afbb3de370ae3f48fc16f91a91b163b6e6524384
Renato Ferreira Soares <35581699+renato130182@users.noreply.github.com>
2021-03-31 22:09:21 -0300

    Aula 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :admins, skip: [:registrations]
+  namespace :admin do 
+    root to: "home#index"
+  end
 end
---
+++ spec/models/admin_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Admin, type: :model do
+  it "Admin is valid" do
+    admin = Admin.new(email: "admin@example.com", password: "teste@123")
+
+    expect(admin.email).to eq("admin@example.com")
+    expect(admin).to be_valid
+  end
+
+  it "Admin is not valid" do
+    admin = Admin.new(email: "admin@example.com")    
+    expect(admin).to_not be_valid
+  end
+end
